60999cf6b94075a264ada2e7866b04e1ef49a379
romanticNomad <satvolume@gmail.com>
2025-12-13 10:23:10 +0000

    race of urls

--- chapter17/src/future.rs
+++ chapter17/src/future.rs
@@ -1,6 +1,6 @@
 use trpl::Html;
 
-pub async fn page_title(url: &str) -> (&str, Option<String>) {
+pub async fn race_title(url: &str) -> (&str, Option<String>) {
     let response_text = trpl::get(url).await.text().await;
     let title = Html::parse(&response_text)
         .select_first("title")
@@ -18,4 +18,10 @@ pub async fn page_title(url: &str) -> (&str, Option<String>) {
 //     }
 // }
 
-
+pub async fn page_title(url: &str) -> Option<String> {
+    let response = trpl::get(url).await;
+    let response_text = response.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(|title| title.inner_html() )
+}
