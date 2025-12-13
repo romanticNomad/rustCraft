549b8aad4c403766a0281026c544e8e0e721eb11
romanticNomad <satvolume@gmail.com>
2025-12-13 07:19:04 +0000

    block-on runtime

--- chapter17/src/main.rs
+++ chapter17/src/main.rs
@@ -2,5 +2,12 @@
 mod future;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    trpl::block_on(async{
+        let url = &args[1];
+        match future::page_title(url).await {
+            Some(title) => println!("The title at {url} is\n{title}"),
+            None => println!("No tittle found at {url}"),
+        }
+    });
 }
