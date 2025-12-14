ea7ba12cbee8aa8670ea8ac505fdd082b4c1810a
romanticNomad <satvolume@gmail.com>
2025-12-14 08:46:27 +0000

    gacefull shutdown

--- chapter17/src/channel.rs
+++ chapter17/src/channel.rs
@@ -15,15 +15,21 @@ pub fn test_channel() {
 pub fn messege() {
     trpl::block_on(async {
         let (tx, mut rx) = trpl::channel();
-        let val = vec!["save".to_string(), "the".to_string(), "environment".to_string(),
-                                "its".to_string(), "getting".to_string(), "late".to_string()];
-        for message in val {
-            tx.send(message).unwrap();
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+        let tx_fut = async move{
+            let val = vec!["save".to_string(), "the".to_string(), "environment".to_string(),
+                                    "its".to_string(), "getting".to_string(), "late".to_string()];
+            for message in val {
+                tx.send(message).unwrap();
+                trpl::sleep(Duration::from_millis(50)).await;
+            }
+        };
 
-        while let Some(message) = rx.recv().await {
-            println!("{}", message);
-        }
+        let rx_fut = async{    
+            while let Some(message) = rx.recv().await {
+                println!("{}", message);
+            }
+        };
+
+        trpl::join(tx_fut, rx_fut).await;
     });
 }
