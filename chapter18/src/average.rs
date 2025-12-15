4c6c524b8ddcbb4b1e4db14cab870a620c386f73
romanticNomad <satvolume@gmail.com>
2025-12-15 21:58:31 +0000

    data tpe creation method

--- chapter18/src/average.rs
+++ chapter18/src/average.rs
@@ -1,9 +1,16 @@
+#[derive(Debug)]
 pub struct AvgCollection {
     list: Vec<i32>,
     average: f64,
 }
 
 impl AvgCollection {
+    pub fn new(list: Vec<i32>) -> AvgCollection {
+        let sum: i32 = list.iter().sum();
+        let average = sum as f64 / list.len() as f64;
+        AvgCollection { list, average }
+    }
+
     pub fn stock(&mut self, element: i32) {
         self.list.push(element);
         self.update_avg();
--- chapter18/src/main.rs
+++ chapter18/src/main.rs
@@ -1,6 +1,12 @@
 #[allow(dead_code)]
 mod average;
+use average::AvgCollection;
 
 fn main() {
-    
+    let mut data1 = AvgCollection::new(vec![5,78,1,2,9]);
+    dbg!(&data1);
+    data1.del();
+    dbg!(&data1);
+    data1.stock(15);
+    dbg!(&data1);
 }
